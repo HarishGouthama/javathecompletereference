6405667835af175b860cf31e8b22f3ade66a332a
HarishGouthama <harish1992.it@gmail.com>
2024-11-17 19:00:54 +0530

    Multi Threading

---
+++ src/com/Chapter11MultiThreadedProgramming/Programs/ThreadDemo.java
@@ -0,0 +1,52 @@
+package com.Chapter11MultiThreadedProgramming.Programs;
+
+//Create a second thread.
+class NewThread implements Runnable
+{
+	Thread t;
+	
+	NewThread()
+	{
+		//Create a new , Second thread
+		t = new Thread(this , "Demo Thread");
+		System.out.println("Child Thread: " + t);
+		t.start(); //Start the thread
+	}
+	
+	//This is the entry point for the second thread.
+	public void run()
+	{
+		try
+		{
+			for(int i = 5 ; i > 0 ; i--)
+			{
+				System.out.println("Child Thread: " + i);
+				Thread.sleep(500);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Child interrupted.");
+		}
+		System.out.println("Exiting Child Thread");
+	}
+}
+public class ThreadDemo {
+	public static void main(String[] args) {
+		new NewThread(); // Create a new Thread
+		
+		try
+		{
+			for(int i = 5 ; i > 0 ; i--)
+			{
+				System.out.println("Main Thread: " + i);
+				Thread.sleep(5000);
+			}
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("Main thread interrupted.");
+		}
+		System.out.println("Main thread exiting.");
+	}
+}
